d1fa5e606d4fd904d3df25623509a035f1301645
juan david marin <juaanmarin2007@gmail.com>
2023-10-19 21:04:49 -0500

    se creo esquema de carpetas y se realiza la conexion a la bd

---
+++ includes/connectionDb.php
@@ -0,0 +1,35 @@
+<?php
+
+    class DB{
+        private $host;
+        private $db;
+        private $user;
+        private $password;
+        private $charset;
+
+        public function __construct(){
+            $this->host = 'localhost';
+            $this->db = '';
+            $this->user = 'root';
+            $this->password = 'Juan50395bg*';
+            $this->charset = 'utf8mb4';
+        }
+
+        public function connect(){
+            try {
+                $connection = "mysql:host=". $this->host . ";dbname=". $this->db . ";charset=". $this->charset;
+                
+                $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES => false];
+                
+                $pdo = new PDO($connection, $this->user, $this->password, $options);
+
+                return $pdo;
+
+            } catch (PDOExeption $e) {
+                print_r("Error connection: ". $e->getMessage());
+            }
+        }
+
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php //include_once("includes/connectionDb.php");  ?>
+<?php //$db = new DB(); $db->connect(); ?>
